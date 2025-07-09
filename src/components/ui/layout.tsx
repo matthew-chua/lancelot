cb7800026015cdf59e3c158437d8b5d5c77f6a18
matthew-chua <mattchua2603@gmail.com>
2025-07-09 23:51:24 +0800

    minor ui fixes

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -4,11 +4,13 @@ import { AppSidebar } from "./app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-screen min-h-screen flex flex-col">
-      <AppSidebar />
-      <Navbar />
-      <div className="flex justify-center w-full py-16 px-8 lg:p-20">
-        <div className="max-w-7xl w-full">{children}</div>
+    <div className="flex flex-col justify-between w-screen min-h-screen">
+      <div className="flex flex-col">
+        <AppSidebar />
+        <Navbar />
+        <div className="flex justify-center w-full py-16 px-8 lg:p-20">
+          <div className="max-w-7xl w-full">{children}</div>
+        </div>
       </div>
       <Footer />
     </div>
