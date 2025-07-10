47be851a7702b39998e1e3953b959447aa699e54
matthew-chua <mattchua2603@gmail.com>
2025-07-10 20:11:57 +0800

    updated sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,82 +1,41 @@
 import { Link } from "react-router-dom";
 
-import {
-  NavigationMenu,
-  // NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  // NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
 import { Button } from "./ui/button";
-import { navigationMenuTriggerStyle } from "./ui/navigation-menu-style";
 import { Menu } from "lucide-react";
 import { useSidebar } from "@/hooks/use-sidebar";
+import { Input } from "./ui/input";
 
 export function Navbar() {
   const { toggleSidebar } = useSidebar();
   return (
-    <NavigationMenu className="flex justify-center items-center fixed top-0 left-0 z-50 w-full p-2 border-b bg-white">
-      <div className="flex justify-between items-center w-full max-w-7xl">
-        <div>
-          <Link to="/">Lancelot</Link>
+    <div className="fixed top-0 left-0 z-10 w-full p-2 border-b bg-white flex flex-col gap-2">
+      <NavigationMenu className="flex justify-center items-center">
+        <div className="flex justify-between items-center w-full max-w-7xl">
+          <div className="flex items-center gap-2">
+            <Button
+              className=""
+              variant="ghost"
+              size="icon"
+              onClick={toggleSidebar}
+            >
+              <Menu />
+            </Button>
+            <Link to="/">Lancelot</Link>
+          </div>
+          <Input
+            placeholder="Search for your next lancelot..."
+            className="w-full hidden lg:block max-w-sm"
+          />
+          <div className="flex items-center gap-2">
+            <Button>Login</Button>
+          </div>
         </div>
-        <NavigationMenuList className="hidden lg:flex">
-          {/* <NavigationMenuItem>
-            <NavigationMenuTrigger>Cat 1</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[200px] gap-4">
-                <li>
-                  <NavigationMenuLink asChild>
-                    <Link to="#">Sub Cat 1</Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link to="#">Sub Cat 2</Link>
-                  </NavigationMenuLink>
-                  <NavigationMenuLink asChild>
-                    <Link to="#">Sub Cat 3</Link>
-                  </NavigationMenuLink>
-                </li>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem> */}
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link to="/cat-2">Cat 2</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link to="/cat-3">Cat 3</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              asChild
-              className={navigationMenuTriggerStyle()}
-            >
-              <Link to="/cat-4">Cat 4</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-        <div className="flex items-center gap-2">
-          <Button>Login</Button>
-          <Button
-            className="lg:hidden"
-            variant="ghost"
-            size="icon"
-            onClick={toggleSidebar}
-          >
-            <Menu />
-          </Button>
-        </div>
-      </div>
-    </NavigationMenu>
+      </NavigationMenu>
+      <Input
+        placeholder="Search for your next lancelot..."
+        className="w-full lg:hidden"
+      />
+    </div>
   );
 }
--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -7,49 +7,132 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
+import { Button } from "./button";
+import { Menu } from "lucide-react";
+import { useSidebar } from "@/hooks/use-sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./collapsible";
 
 // Menu items.
 const items = [
   {
-    title: "Cat 1",
+    title: "Fitness Coaches",
     url: "/test",
+    subItems: [
+      {
+        title: "Weight Loss",
+        url: "/test",
+      },
+      {
+        title: "Muscle Gain",
+        url: "/test",
+      },
+    ],
   },
   {
-    title: "Cat 2",
+    title: "Skils Coaches",
     url: "/test",
+    subItems: [
+      {
+        title: "Music",
+        url: "/test",
+      },
+      {
+        title: "Art",
+        url: "/test",
+      },
+    ],
   },
   {
-    title: "Cat 3",
+    title: "Task Assistance",
     url: "/test",
+    subItems: [
+      {
+        title: "Sub 1",
+        url: "/test",
+      },
+      {
+        title: "Sub 2",
+        url: "/test",
+      },
+    ],
   },
   {
-    title: "Cat 4",
+    title: "Home Businesses",
     url: "/test",
+    subItems: [
+      {
+        title: "Cafes",
+        url: "/test",
+      },
+      {
+        title: "Bakeries",
+        url: "/test",
+      },
+      {
+        title: "Private Dining",
+        url: "/test",
+      },
+    ],
   },
   {
     title: "Cat 5",
     url: "/test",
+    subItems: [
+      {
+        title: "Sub 1",
+        url: "/test",
+      },
+      {
+        title: "Sub 2",
+        url: "/test",
+      },
+    ],
   },
 ];
 
 export function AppSidebar() {
+  const { toggleSidebar } = useSidebar();
   return (
-    <Sidebar className="lg:hidden">
+    <Sidebar className="z-20">
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Lancelot</SidebarGroupLabel>
+          <div className="flex justify-between items-center">
+            <SidebarGroupLabel>Lancelot</SidebarGroupLabel>
+            <Button variant="ghost" size="icon" onClick={toggleSidebar}>
+              <Menu />
+            </Button>
+          </div>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      {/* <item.icon /> */}
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                <Collapsible defaultOpen className="group/collapsible">
+                  <SidebarMenuItem>
+                    <CollapsibleTrigger asChild>
+                      <SidebarMenuButton>
+                        <span>{item.title}</span>
+                      </SidebarMenuButton>
+                    </CollapsibleTrigger>
+                    <CollapsibleContent>
+                      <SidebarMenuSub>
+                        {item.subItems.map((subItem) => (
+                          <SidebarMenuSubItem>
+                            <SidebarMenuSubButton asChild>
+                              <a href={subItem.url}>{subItem.title}</a>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        ))}
+                      </SidebarMenuSub>
+                    </CollapsibleContent>
+                  </SidebarMenuItem>
+                </Collapsible>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
